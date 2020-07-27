d0ce9fe177c23cc8feed241e0a630b79af653d58
r7rohan <rohanmallick711@gmail.com>
2020-07-27 22:41:58 +0530

    Update index.js

--- Autoencoder/index.js
+++ Autoencoder/index.js
@@ -22,137 +22,124 @@ function createConvModel(n_layers,n_units,hidden) {
   this.img_shape = [28,28];
   this.img_units = this.img_shape[0] * this.img_shape[1];
   // build the encoder
-  var i = tf.input({shape: this.img_shape})
-  var h = tf.layers.flatten().apply(i)
+  var i = tf.input({shape: this.img_shape});
+  var h = tf.layers.flatten().apply(i);
 
   for (var j=0; j<this.n_layers; j++) {
-    var h = tf.layers.dense({units: this.n_units, activation:'relu'}).apply(h)              //n hidden
+    var h = tf.layers.dense({units: this.n_units, activation:'relu'}).apply(h);           //n hidden
   }
 
-  var o = tf.layers.dense({units: this.latent_dim}).apply(h)                                //1 final
+  var o = tf.layers.dense({units: this.latent_dim}).apply(h);                               //1 final
   this.encoder = tf.model({inputs: i, outputs: o});
 
   // build the decoder
-  var i = h = tf.input({shape: this.latent_dim})
+  var i = h = tf.input({shape: this.latent_dim});
   for (var j=0; j<this.n_layers; j++) {                                                     //n hidden
-    var h = tf.layers.dense({units: this.n_units, activation:'relu'}).apply(h)
+    var h = tf.layers.dense({units: this.n_units, activation:'relu'}).apply(h);
   }
-  var o = tf.layers.dense({units: this.img_units}).apply(h)                                 //1 final
-  var o = tf.layers.reshape({targetShape: this.img_shape}).apply(o)
-  this.decoder = tf.model({inputs: i, outputs: o})
+  var o = tf.layers.dense({units: this.img_units}).apply(h)   ;                              //1 final
+  var o = tf.layers.reshape({targetShape: this.img_shape}).apply(o);
+  this.decoder = tf.model({inputs: i, outputs: o});
 
   // stack the autoencoder
-  var i = tf.input({shape: this.img_shape})
-  var z = this.encoder.apply(i)                                                             //z is hidden code
+  var i = tf.input({shape: this.img_shape});
+  var z = this.encoder.apply(i);                                                          //z is hidden code
 
-  var o = this.decoder.apply(z)
-  this.auto = tf.model({inputs: i, outputs: o})
+  var o = this.decoder.apply(z);
+  this.auto = tf.model({inputs: i, outputs: o});
 
 }
-let epochs;
+let epochs=0,trainEpochs,batch;
+var trainData;
+var testData;
+var b;var model;
 
 async function train(model) {
 
-  const e=document.getElementById('batchsize')
-  const batchSize = Number(e.value)
-
-
-  // Leave out the last 15% of the training data for validation, to monitor
-  // overfitting during training.
-  const validationSplit = 0;
+  const e=document.getElementById('batchsize');
+  batch = Number(e.value);
+  const validationSplit = Number(0);
 
   // Get number of training epochs from the UI.
-  const element=document.getElementById('train-epochs')
-  const trainEpochs = Number(element.value)
-  const lr = Number(document.getElementById('lr').value)
+  const element=document.getElementById('train-epochs');
+  trainEpochs = Number(element.value);
+  const lr = Number(document.getElementById('lr').value);
 
-  const ele=document.getElementById('new')
-  ele.innerHTML="Training..."
-  epochs=Number(epochs)+Number(trainEpochs)
 
-  // We'll keep a buffer of loss and accuracy values over time.
-  let trainBatchCount = 0;
+  epochs=Number(epochs)+Number(trainEpochs);
 
-  const trainData = data.getTrainData();
-  const testData = data.getTestData();
+  const y=trainData.xs.reshape([-1,28,28]);
 
-  const totalNumBatches =
-      Math.ceil(trainData.xs.shape[0] * (1 - validationSplit) / batchSize) *
-      trainEpochs;
-
-  // During the long-running fit() call for model training, we include
-  // callbacks, so that we can plot the loss and accuracy values in the page
-  // as the training progresses.
-  const y=trainData.xs.reshape([-1,28,28])
-
-  model.auto.compile({optimizer: 'adam', loss: 'meanSquaredError', lr: lr})
+  model.auto.compile({optimizer: 'adam', loss: 'meanSquaredError', lr: lr});
+  const onBatchEnd=loadbar;
 
   await model.auto.fit(y, y, {
-    batchSize,
-    validationSplit,
+    batchSize:batch,
+    validationSplit:validationSplit,
     epochs: trainEpochs,
-
+    callbacks: [{
+    onBatchEnd: loadbar,}],
   });
 await showPredictions(model,epochs);                                      //Trivial Samples of autoencoder
 
 }
 
-/**
- *
- */
+
+
 async function showPredictions(model,epochs) {                              //Trivial Samples of autoencoder
   const testExamples = 10;
   const examples = data.getTestData(testExamples);
 
-
   tf.tidy(() => {
     const output = model.auto.predict(examples.xs.reshape([-1,28,28]));
-
-
 
     ui.showTestResults(examples.xs.reshape([-1,28,28]),output,epochs);
   });
 }
 
 
-
-
-
 var data,vis=Number(500);
 async function run(){
   data = new MnistData();
   await data.load();
+  trainData = data.getTrainData();
+  testData = data.getTestData();
 }
 
-var model;
+
 async function load() {
-  const n_units=document.getElementById('n_units').value
-  const n_layers=document.getElementById('n_layers').value
-  const hidden=document.getElementById('hidden').value
+  var ele=document.getElementById('barc');
+  ele.style.display="none";
+  const n_units=document.getElementById('n_units').value;
+  const n_layers=document.getElementById('n_layers').value;
+  const hidden=document.getElementById('hidden').value;
   model = new createConvModel(n_layers,n_units,hidden);
   const elem=document.getElementById('new')
   elem.innerHTML="Model Created!!!"
   epochs=0;
   vis=Number(document.getElementById('vis').value);
 }
+
+load();
+
 async function runtrain(){
+  var ele=document.getElementById('barc');
+  ele.style.display="block";
+  var elem=document.getElementById('new');
+  elem.innerHTML="";
+  b=0;
   await train(model);
   vis=Number(document.getElementById('vis').value);
 }
 
+function loadbar(){
+  var element=document.getElementById("bar");
+  element.style.width=Math.min(Math.ceil((b*100*batch)/(trainEpochs*55000)),100)+'%';
+  element.innerHTML=Math.min(Math.ceil((b*100*batch)/(trainEpochs*55000)),100)+'%';
+  b++;
+  console.log(b);
+}
 
-// This is our main function. It loads the MNIST data, trains the model, and
-// then shows what the model predicted on unseen test data.
-
-load()                                                       //load model
-
-
-
-
-
-
-var container=document.getElementById('cn')               //plot2d area
-const canvas=document.getElementById('celeba-scene')
 
 
 function normaltensor(prediction){
@@ -170,9 +157,15 @@ function normal(prediction){
   return prediction;
 }
 
+
+var container=document.getElementById('cn');               //plot2d area
+const canvas=document.getElementById('celeba-scene');
+const mot=document.getElementById('mot');
+var cont=mot.getContext('2d');
+
 function sample(obj) {                                    //plotting
-  obj.x = (obj.x - 0.5) * vis;
-  obj.y = (obj.y - 0.5) * vis;
+  obj.x = (obj.x) * vis;
+  obj.y = (obj.y) * vis;
   // convert 10, 50 into a vector
   var y = tf.tensor2d([[obj.x, obj.y]]);
   // sample from region 10, 50 in latent space
@@ -190,19 +183,47 @@ function sample(obj) {                                    //plotting
   tf.browser.toPixels(prediction, canvas);
 }
 
+var mouse={x:0,y:0};
+sample(mouse);
+cont.fillStyle = "#DDDDDD";
+cont.fillRect(0,0,mot.width,mot.height);
+mot.addEventListener('mousemove', function(e) {
+    mouse.x = (e.pageX - this.offsetLeft)*3.43;
+    mouse.y = (e.pageY - this.offsetTop)*1.9;
+}, false);
+
+mot.addEventListener('mousedown', function(e) {
+    mot.addEventListener('mousemove', on, false);
+}, false);
+
+mot.addEventListener('mouseup', function() {
+    mot.removeEventListener('mousemove', on, false);
+}, false);
+
+var on= function() {
+  cont.fillStyle = "#BBBBBB";
+    cont.fillRect(0,0,mot.width,mot.height);
+    cont.fillStyle="#000000";
+    cont.fillRect(mouse.x-10,mouse.y-10, 40, 20);
+    sample(mouse);
+};
+
+
+
+
+
 
 
 function plot2d(){
   load();
-  const decision=Number(document.getElementById("hidden").value)
+  const decision=Number(document.getElementById("hidden").value);
   if(decision===Number(2)){
-    canvas.style.display="inline-block";
-  window.c2d = new Controls2D({ onDrag: sample, container: container });
-  sample({x:0,y:0});}
+    container.style.display="block";
+  }
   else {  var context = canvas.getContext('2d');
     context.clearRect(0, 0, canvas.width, canvas.height);
-    canvas.style.display="none";                                                  //clearing canvas for higher dimensions
-    container.innerHTML="";}
+    container.style.display="none";                                                  //clearing canvas for higher dimensions
+    }
 }
 
 
@@ -212,6 +233,8 @@ const element=document.getElementById('train')
 element.addEventListener('click', runtrain);
 
 document.addEventListener('DOMContentLoaded', run);
+document.addEventListener('DOMContentLoaded',plot2d);                                                  //load model
+
 
 
 
@@ -221,7 +244,7 @@ document.addEventListener('DOMContentLoaded', run);
 const canv=document.getElementById('canv');
 const outcanv=document.getElementById('outcanv');
 var ct = outcanv.getContext('2d');
-// get canvas 2D context and set him correct size
+
 var ctx = canv.getContext('2d');
 
 function clear(){
@@ -274,7 +297,7 @@ clear();
 
 function rundraw(){
   var sm=tf.browser.fromPixels(canv,1);
-  sm=sm.resizeNearestNeighbor([28,28]).toFloat().reshape([-1,28,28]);
+  sm=sm.toFloat().resizeNearestNeighbor([28,28]).reshape([-1,28,28]);
   sm=normal(sm);
 
   var pr=model.auto.predict(sm).dataSync();
